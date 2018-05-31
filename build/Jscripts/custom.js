6b520632d6b979e5088969bf0dbf760b6523072a
longht <longht@vng.com.vn>
2018-05-31 09:01:38 +0700

    Update fix 30/05/2018

--- build/Jscripts/custom.js
+++ build/Jscripts/custom.js
@@ -6,17 +6,17 @@ $(function() {
     // toggle header search
     $('.button--search').click(function() {
         $(this).toggleClass('is-active');
-        $('.menu-open').slideUp('fast');
         $(".search-open").slideToggle();
-        $('.button--menu').removeClass('is-active');
+        // $('.menu-open').slideUp('fast');
+        // $('.button--menu').removeClass('is-active');
     });
     // toggle header search
-    $('.button--menu').click(function() {
-        $(this).toggleClass('is-active');
-        $('.search-open').slideUp('fast');
-        $(".menu-open").slideToggle();
-        $('.button--search').removeClass('is-active');
-    });
+    // $('.button--menu').click(function() {
+    //     $(this).toggleClass('is-active');
+    //     $('.search-open').slideUp('fast');
+    //     $(".menu-open").slideToggle();
+    //     $('.button--search').removeClass('is-active');
+    // });
     // index slider
     $('.zone--highlight .slider').bxSlider({
         nextText: '',
